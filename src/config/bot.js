b575cdecf0df422139570948e09a00cbbc655b49
clanvfx5-hash <clanvfx5@gmail.com>
2026-06-16 20:54:55 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -11,7 +11,7 @@ export const botConfig = {
   // - "invisible" = appears offline
   presence: {
     // Current online state shown on Discord.
-    status: "idle",
+    status: "online",
 
     // Activity lines shown under the bot name.
     // `type` number mapping from Discord:
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "UPDATING!",
+        name: "Hey, Im Star!",
         // Activity type number (0 = Playing).
         type: 2,
       },
